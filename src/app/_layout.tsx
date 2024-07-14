1c629d77ce86c0e7c550c1343f3db41920005ebf
“Ewin” <ewinoaks@gmail.com>
2024-07-14 17:12:21 +0800

    ExpoRouter & TabNavigation

---
+++ src/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function RootLayout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
+
+//_layout file to Share ui Elements and Logic between multiple screens
+//and its going to wrap every screen on pur Apllications
+//The layout simply asking what to render? whether its <Tabs /> <Slot /> etc..
+//and if its <Stack /> then youre going to render and actual Navigator,  i mean if its index.tsx about.tsx etc...
+
+//Slot -is like children, take everything an render it here in _layout
+//import { Slot }from 'expo-router
---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+import { Redirect } from "expo-router";
+//32:37
+export default function Home() {
+  return <Redirect href="/(tabs)" />;
+}
+//<Link href="/about">Go to About Page</Link>;
